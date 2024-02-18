d3dc41bd9cfb02cdb86ec9a4be6bb7a637c639de
theckl <thomas.eckl@liv.ac.uk>
2024-02-18 18:18:19 +0000

    Create logic.lean

---
+++ CHoTT4/init/logic.lean
@@ -0,0 +1,33 @@
+import CHoTT4.init.path
+
+universe u
+
+namespace hott
+
+def Empty.elim {A : Type _} : Empty -> A :=
+  fun h : Empty => Empty.rec h
+
+/- not -/
+def Not (A : Type u) := A → Empty
+
+prefix:75 "¬" => hott.Not
+
+/- inequality -/
+def NEq {A : Type u} (a b : A) := Not (Eq a b)
+
+infix:55 (priority := high) " ≠ " => NEq
+
+def NEq.inverse {A : Type u} {a b : A} (np : a ≠ b) : b ≠ a :=
+  fun q => np q⁻¹
+
+postfix:max "⁻¹" => NEq.inverse
+
+/- decidable -/
+class inductive Decidable (P : Type u) : Type u
+| isTrue  :  P → Decidable P
+| isFalse : ¬P → Decidable P
+
+abbrev DecidableEq (A : Type _) :=
+  (a b : A) -> Decidable (a = b)
+
+end hott
