57ea3431a6601cb3a69413b4c3701877b255751f
theckl <thomas.eckl@liv.ac.uk>
2024-03-02 09:06:31 +0000

    Update path.lean

--- CHoTT4/init/path.lean
+++ CHoTT4/init/path.lean
@@ -1,5 +1,5 @@
 /- Ported from HoTT3 -/
-import CHoTT4.init.path0
+import CHoTT4.init.Path0
 
 universe u v
 
@@ -18,13 +18,13 @@ def Eq.concat {A : Type u} {a b c : A} : a = b -> b = c -> a = c
 
 infix:75 " ⬝ " => Eq.concat
 
-def transport {P : A → Type v} {x y : A} : (x = y) -> (P x) -> P y
+def Transport {P : A → Type v} {x y : A} : (x = y) -> (P x) -> P y
 | Eq.refl => fun u => u
 
-notation:65 q:65 " ▸ " u => transport q u
-notation:65 q:65 " ▸[" P:65 "] " u => @transport _ P _ _ q u
+notation:65 q:65 " ▸ " u => Transport q u
+notation:65 q:65 " ▸[" P:65 "] " u => @Transport _ P _ _ q u
 
-def ap {A : Type u} {B : Type v} (f : A → B) {x y : A} :
+def Ap {A : Type u} {B : Type v} (f : A → B) {x y : A} :
   (p : x = y) -> f x = f y
 | Eq.refl => IdP
 
