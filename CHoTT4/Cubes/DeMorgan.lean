f25759c4448326461325f3ac4567e496920cc9a2
theckl <thomas.eckl@liv.ac.uk>
2024-03-13 22:52:13 +0000

    Create DeMorgan.lean

---
+++ CHoTT4/Cubes/DeMorgan.lean
@@ -0,0 +1,23 @@
+import CHoTT4.Cubes.Direction
+
+universe u
+
+namespace hott
+
+open Dir
+
+namespace dM
+
+/- We introduce the free deMorgan algebra over a direction set by describing the elements and
+   algebraic operations explicitly and then proving the axioms and (universal) properties
+   of a (free) deMorgan algebra, instead of constructing it as an instance of a general
+   structure and construction, with the axioms and properties implied.
+
+   The reason is that we do not need other (free) de Morgan algebras, with the possible
+   exception of subalgebras of the free DeMorgan algebras over direction sets. In that way,
+   we also follow the model of introducing natural numbers: They form certainly a
+   semiring, but the properties of their algebraic operations are proven from scratch. -/
+
+end dM
+
+end hott
