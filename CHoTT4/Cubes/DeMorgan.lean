88ef1100725e06516097b51a32c0481de51fab91
theckl <thomas.eckl@liv.ac.uk>
2024-03-19 22:48:02 +0000

    Update DeMorgan.lean

--- CHoTT4/Cubes/DeMorgan.lean
+++ CHoTT4/Cubes/DeMorgan.lean
@@ -8,10 +8,10 @@ open Dir
 
 namespace dM
 
-/- We introduce the free deMorgan algebra over a direction set by describing the elements and
-   algebraic operations explicitly and then proving the axioms and (universal) properties
-   of a (free) deMorgan algebra, instead of constructing it as an instance of a general
-   structure and construction, with the axioms and properties implied.
+/- We introduce the free deMorgan algebra over a direction set by describing the elements
+   and algebraic operations explicitly and then proving the axioms and (universal)
+   properties of a (free) deMorgan algebra, instead of constructing it as an instance of
+   a general structure and construction, with the axioms and properties implied.
 
    The reason is that we do not need other (free) de Morgan algebras, with the possible
    exception of subalgebras of the free DeMorgan algebras over direction sets. In that way,
